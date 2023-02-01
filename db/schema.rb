17b53a86d9051824d6943359b52afd8704f78d6d
Suyash chaudhary <yashusuyashchaudhary@gmail.com>
2023-02-01 23:07:07 +0530

    New user form added

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_140656) do
+ActiveRecord::Schema.define(version: 2023_01_23_165109) do
 
   create_table "articles", force: :cascade do |t|
-   t.string "title"
-   t.text "description"
-   t.datetime "created_at"
-   t.datetime "updated_at"
-   t.integer "user_id"
- end
- create_table "users", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "password"
   end
 
 end
