d9d91461bbb4fae92b497b4dc9308f6bdb98565b
Suyash chaudhary <yashusuyashchaudhary@gmail.com>
2023-02-01 16:34:06 +0530

    Sigh up form added

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_171808) do
+ActiveRecord::Schema.define(version: 2020_04_07_140656) do
+
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
+   t.string "title"
+   t.text "description"
+   t.datetime "created_at"
+   t.datetime "updated_at"
+   t.integer "user_id"
+ end
+ create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
