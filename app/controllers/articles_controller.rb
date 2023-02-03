b8fe943f00fd3754a27ead331fee3addc700cc54
Suyash chaudhary <yashusuyashchaudhary@gmail.com>
2023-02-03 17:31:52 +0530

    Added pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,23 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-  end
-  def index
-    @articles = Article.all
-  end
-  def new
+   end
+
+   def index
+     @articles = Article.paginate(page: params[:page], per_page: 5)
+   end
+
+   def new
     @article = Article.new
   end
   def edit
   end
-
-   def create
-     @article = Article.new(article_params)
-     @article.user = User.first
-     if @article.save
-       flash[:notice] = "Article was created successfully."
-       redirect_to @article
+  def create
+    @article = Article.new(article_params)
+    @article.user = User.first
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
     else
       render 'new'
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
-   def index
-     @users = User.all
-   end
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
 
   def new
-    @user = User.new
-  end
+   @user = User.new
+ end
 
  def edit
    @user = User.find(params[:id])
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
    if @user.update(user_params)
      flash[:notice] = "Your account information was successfully updated"
-     redirect_to articles_path
+     redirect_to @user
    else
      render 'edit'
    end
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
      render 'new'
    end
  end
-
+ 
  private
  def user_params
    params.require(:user).permit(:username, :email, :password)
