56c3adf268d8fb1d18967547d0a155db94552314
Suyash chaudhary <yashusuyashchaudhary@gmail.com>
2023-02-03 12:32:40 +0530

    Added user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,18 @@ class UsersController < ApplicationController
     @articles = @user.articles
   end
 
+   def index
+     @users = User.all
+   end
+
   def new
     @user = User.new
   end
+
  def edit
    @user = User.find(params[:id])
  end
+
  def update
    @user = User.find(params[:id])
    if @user.update(user_params)
@@ -20,6 +26,7 @@ class UsersController < ApplicationController
      render 'edit'
    end
  end
+
  def create
    @user = User.new(user_params)
    if @user.save
@@ -29,6 +36,7 @@ class UsersController < ApplicationController
      render 'new'
    end
  end
+
  private
  def user_params
    params.require(:user).permit(:username, :email, :password)
