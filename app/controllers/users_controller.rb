dbd90678982fa8fdf1a81099e6d27000113be4c3
Suyash chaudhary <yashusuyashchaudhary@gmail.com>
2023-02-08 21:55:40 +0530

    Action restrictions, Delete user, admin user functionalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
@@ -35,6 +38,13 @@ class UsersController < ApplicationController
    end
  end
  
+ def destroy
+  @user.destroy
+  session[:user_id] = nil if @user == current_user
+  flash[:notice] = "User account and all associated articles deleted successfully"
+  redirect_to users_path
+ end
+
  private
  def user_params
    params.require(:user).permit(:username, :email, :password)
@@ -44,4 +54,10 @@ class UsersController < ApplicationController
   @user = User.find(params[:id])
  end
 
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own account"
+      redirect_to @user
+    end
+  end
 end
